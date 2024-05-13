8dee56371beab67125ccfec2079ca9ecdfda614f
yangdajeong <yangdajeong3@gmail.com>
2024-05-13 11:11:42 +0900

    크래프팅 최종완성 백업

---
+++ Assets/0YDJ/Scripts/CraftManual.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Craft
+{
+    public string craftName; // 이름
+    public GameObject go_prefab; // 실제 설치 될 프리팹
+    public GameObject go_PreviewPrefab; // 미리 보기 프리팹
+}
+
+public class CraftManual : MonoBehaviour
+{
+    //public static CraftManual instance;
+
+    private bool isActivated = false;  // CraftManual UI 활성 상태
+    private bool isPreviewActivated = false; // 미리 보기 활성화 상태
+
+
+    [SerializeField] GameObject go_BaseUI; // 기본 베이스 UI
+
+
+    [SerializeField] Craft[] craft_fire;  // 인벤토리에 있는 슬롯들
+
+    private GameObject go_Preview; // 미리 보기 프리팹을 담을 변수
+    private GameObject go_Prefab; // 실제 생성될 프리팹을 담을 변수 
+
+
+    public Vector3 Pointer;
+    //public Vector3 pointer { get { return Pointer; } }
+
+    private RaycastHit hitInfo;
+
+    [SerializeField] LayerMask GroundLayer;
+
+    [SerializeField] LayerMask IgnoreLayer;
+
+    [SerializeField] float range;
+
+    //private void Awake()
+    //{
+    //    if(CraftManual.instance == null)
+    //    {
+    //        CraftManual.instance = this;
+    //    }
+    //}
+
+    private void PointerPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //카메라에서 레이 쏘기
+        if (Physics.Raycast(ray, out hitInfo, range, IgnoreLayer | GroundLayer)) //레이캐스트로 그라운드 체크(range로 범위 조정 가능)                      
+        {
+            Pointer = hitInfo.point;
+            Pointer.y = hitInfo.transform.position.y + hitInfo.transform.up.y;
+            //Debug.Log($"Pointer.y : {Pointer.y}");
+        }
+    }
+
+
+    public void SlotClick(int _slotNumber) //슬럿 클릭시 프리뷰 프리펩 생성
+    {
+        go_Preview = Instantiate(craft_fire[_slotNumber].go_PreviewPrefab, Pointer, Quaternion.identity);
+        go_Prefab = craft_fire[_slotNumber].go_prefab;
+        isPreviewActivated = true;
+        go_BaseUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        PointerPosition();
+
+        if (Input.GetKeyDown(KeyCode.Tab) && !isPreviewActivated) //인벤토리 창 열기
+            Window();
+
+        if (isPreviewActivated) // 프리뷰 상태 중 프리뷰 프리펩 계속 업데이트
+            PreviewPositionUpdate();
+
+        if (Input.GetButtonDown("Fire1")) // 짓기
+            Build();
+
+        if (Input.GetKeyDown(KeyCode.Escape)) // 짓는 중에 캔슬
+            Cancel();
+
+    }
+
+
+
+    private void PreviewPositionUpdate() // 미리보기 프리펩 위치 업데이트
+    {
+
+        if (Input.GetKey(KeyCode.R) && Input.GetKey(KeyCode.LeftShift)) // 로테이션
+        {
+            go_Preview.transform.Rotate(0, -2f, 0);
+        }
+        else if(Input.GetKey(KeyCode.R))
+        {
+            go_Preview.transform.Rotate(0, 2f, 0);
+        }
+
+        go_Preview.transform.position = Pointer;
+
+    }
+
+
+    private void Build() // 짓기
+    {
+        if (isPreviewActivated && go_Preview.GetComponent<PreviewObject>().isBuildable())
+        {
+            //Instantiate(go_Prefab, Pointer, Quaternion.identity);
+            Instantiate(go_Prefab, Pointer, go_Preview.transform.rotation);
+            Destroy(go_Preview);
+            isActivated = false;
+            isPreviewActivated = false;
+            go_Preview = null;
+            go_Prefab = null;
+        }
+    }
+
+    private void Window()
+    {
+        if (!isActivated)
+            OpenWindow();
+        else
+            CloseWindow();
+    }
+
+    private void OpenWindow()
+    {
+        isActivated = true;
+        go_BaseUI.SetActive(true);
+    }
+
+    private void CloseWindow()
+    {
+        isActivated = false;
+        go_BaseUI.SetActive(false);
+    }
+
+    private void Cancel()
+    {
+        if (isPreviewActivated)
+            Destroy(go_Preview);
+
+        isActivated = false;
+        isPreviewActivated = false;
+
+        go_Preview = null;
+        go_Prefab = null;
+
+        go_BaseUI.SetActive(false);
+    }
+}
---
+++ Assets/0YDJ/Scripts/PreviewObject.cs
@@ -0,0 +1,206 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UIElements;
+
+public class PreviewObject : MonoBehaviour
+{
+    public List<Collider> colliderList = new List<Collider>(); // 충돌한 오브젝트들 저장할 리스트
+
+    [SerializeField] int layerGround = 31; // 지형 레이어 (무시하게 할것)
+    [SerializeField] int IGNORE_RAYCAST_LAYER = 30; // ignore_raycast (무시하게 할것)
+
+    [SerializeField] Material blue;
+    [SerializeField] Material red;
+    [SerializeField] float range = 0.5f;
+
+    private RaycastHit hitInfo;
+    [SerializeField] LayerMask IgnoreLayer;
+
+    private Renderer newMaterials;
+
+    private Vector3 Vel;
+
+
+
+    private void Start()
+    {
+        newMaterials = gameObject.GetComponent<Renderer>();
+
+        Vel = transform.localScale; // 오버랩 크기 줄이기
+        Vel.y -= 0.9f;
+        Vel.x -= 0.5f;
+        Vel.z -= 0.5f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"colliderList.Count : {colliderList.Count}");
+        ChangeColor();
+        //Debug.Log($"colliderList : {colliderList.}");
+    }
+
+    private void ChangeColor()
+    {
+        if (colliderList.Count > 0)
+            SetColor(red);
+        else
+            SetColor(blue);
+    }
+
+    private void SetColor(Material mat)
+    {
+         newMaterials.material = mat;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(transform.position, Vel);
+    }
+
+
+    private void OnTriggerStay(Collider other)               //**레이캐스트
+    {
+        if (other.gameObject.layer == 30)
+        {
+            Collider[] colliders = Physics.OverlapBox(transform.position, Vel);
+            foreach (Collider collider in colliders)
+            {
+                Debug.Log(collider.ToString());
+                if (collider.gameObject.layer == 30)
+                {
+                    if (!colliderList.Contains(other))
+                    {
+                        Debug.Log("오버랩에 닿아서 지을 수 없다");
+                        colliderList.Add(other);
+                    }
+                    return;
+                }
+
+                Debug.Log("아래 블럭이 있어서 지을 수 있다");
+                colliderList.Remove(other);
+                
+            }
+        }
+
+        if(other.gameObject.layer != layerGround && other.gameObject.layer != 30) //그라운드 레이어 외에는 콜리더 리스트에 추가
+        {
+
+            if (!colliderList.Contains(other)) //이미 리스트에 똑같은 other물질이라면 더이상 추가 안하기
+            {
+                Debug.Log("Obstacle 감지, 리스트 Add");
+                colliderList.Add(other);
+            }
+        }
+    }
+
+
+
+
+
+
+    //********************************************** 다정이의 시행착오들 ***********************************************//
+
+    //private void OnTriggerEnter(Collider other)                   //**박스캐스트
+    //{
+    //    if (other.gameObject.layer == 30) //IGNORE_RAYCAST_LAYER를 쓰면 이상하게 오류남
+    //    {
+    //        //Debug.DrawRay(transform.position, Vector3.down,red.color, 100f);
+    //        if (Physics.BoxCast(transform.position, transform.lossyScale / 2, Vector3.down, out hitInfo, transform.rotation, 10f , IgnoreLayer)) // 레이캐스트를 밑으로 쏘아서 탑쌓기 기능 구현
+    //        {
+
+    //            Debug.Log("레이캐스트에 닿아서 리턴");
+    //            if(colliderList.Contains(hitInfo.collider))
+    //            {
+    //                colliderList.Remove(hitInfo.collider); 
+    //            }
+    //            return;
+    //        }
+    //        colliderList.Add(other);
+    //    }
+    //    else if (other.gameObject.layer != layerGround) //그라운드 레이어 외에는 콜리더 리스트에 추가
+    //    {
+    //        //Debug.Log("Add");
+    //        colliderList.Add(other);
+    //    }
+    //}
+
+    //private void OnTriggerEnter(Collider other)                //**포인터 수치로 판단하기
+    //{
+    //    if (other.gameObject.layer == 30) //IGNORE_RAYCAST_LAYER를 쓰면 이상하게 오류남
+    //    {
+    //        //Debug.Log($"Pointer.y : {CraftManual.instance.Pointer.y}");
+    //        if (CraftManual.instance.Pointer.y > 1)
+    //        {
+    //            Debug.Log("포인터 수치 올라가서 리턴");
+    //            return;
+    //        }
+    //        colliderList.Add(other);
+    //    }
+    //    else if (other.gameObject.layer != layerGround) //그라운드 레이어 외에는 콜리더 리스트에 추가
+    //    {
+    //        //Debug.Log("Add");
+    //        colliderList.Add(other);
+    //    }
+    //}
+
+
+
+    //private void OnTriggerEnter(Collider other)                //**오버랩
+    //{
+    //    if (other.gameObject.layer == 30) //IGNORE_RAYCAST_LAYER를 쓰면 이상하게 오류남
+    //    {
+    //        Collider[] colliders = Physics.OverlapBox(transform.position, new Vector3(0.5f, 5 , 0.5f));
+    //        foreach (Collider collider in colliders)
+    //        {
+    //            if (collider.gameObject.layer == LayerMask.NameToLayer("Ignore"))
+    //            {
+
+    //                Debug.Log("오버랩에 닿아서 리턴");
+    //                if (colliderList.Contains(other))
+    //                {
+    //                    colliderList.Remove(other);
+    //                }
+    //                return;
+    //            }
+    //        }
+    //        colliderList.Add(other);
+    //    }
+    //    else if (other.gameObject.layer != layerGround) //그라운드 레이어 외에는 콜리더 리스트에 추가
+    //    {
+    //        //Debug.Log("Add");
+    //        colliderList.Add(other);
+    //    }
+    //}
+
+    //void OnDrawGizmos() // 범위 그리기
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireCube(transform.position, new Vector3(0, -5, 0));
+
+    //}
+    //****************************************************************************************************//
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer != layerGround)
+        {
+            //Debug.Log("Remove");
+            colliderList.Remove(other);
+        }
+    }
+    public bool isBuildable()
+    {
+        return colliderList.Count == 0;
+    }
+}
