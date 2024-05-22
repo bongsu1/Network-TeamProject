4de0f28d7908c08075807d9d17aaba2b7187379c
bongsu <rlaqhdtn22@gmail.com>
2024-05-22 13:57:17 +0900

    feat : 닭 도망상태 구현
    주변에 플레이어가 다가오면 반대방향으로 도망
    뒤에서 접근시 발각되지 않음

--- Assets/1bongsu/Scripts/Animal/Animal.cs
+++ Assets/1bongsu/Scripts/Animal/Animal.cs
@@ -1,4 +1,5 @@
 using Photon.Pun;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,19 +8,32 @@ public class Animal : MonoBehaviourPun, IPunObservable
     public enum State { Idle, Roam, Run }
     protected StateMachine<State> stateMachine = new StateMachine<State>();
 
-    protected State curState;
+    [SerializeField] protected State curState; // 디버그 확인용
     public State CurState { set { curState = value; } }
 
-    [SerializeField] protected Rigidbody rigid;
-    public Rigidbody Rigid { get { return rigid; } }
-
+    [Header("Component")]
     [SerializeField] protected NavMeshAgent agent;
     public NavMeshAgent Agent { get { return agent; } }
 
     [SerializeField] protected Animator animator;
     public Animator Animator { get { return animator; } }
 
+    [Header("Player Search")]
+    [SerializeField] LayerMask targetLayer;
+    [SerializeField] float searchRadius;
+    [SerializeField] float searchAngle;
+
+    private float cosRange;
+
+    private Transform target;
+    public Transform Target { get { return target; } set { target = value; } }
+
     protected bool isMaster; // stateMachine.CurState에 할당해주는 속도가 조금 늦어서 추가하는 조건
+
+    protected virtual void Awake()
+    {
+        cosRange = Mathf.Cos(Mathf.Deg2Rad * searchAngle);
+    }
 
     protected virtual void Update()
     {
@@ -45,6 +59,40 @@ public class Animal : MonoBehaviourPun, IPunObservable
         stateMachine.LateUpdate();
     }
 
+    private Collider[] colliders = new Collider[10];
+    private void Search()
+    {
+        int size = Physics.OverlapSphereNonAlloc(transform.position, searchRadius, colliders, targetLayer);
+        if (size > 0)
+        {
+            foreach (Collider collider in colliders)
+            {
+                if (collider == null)
+                    continue;
+
+                PlayerController target = collider.GetComponent<PlayerController>();
+                if (target == null)
+                    continue;
+
+                Vector3 toTargetDir = (target.transform.position - transform.position).normalized;
+                if (Vector3.Dot(toTargetDir, transform.forward) < cosRange)
+                    continue;
+
+                this.target = target.transform;
+                break;
+            }
+        }
+    }
+
+    public IEnumerator SearchRoutine()
+    {
+        while (true)
+        {
+            Search();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
@@ -55,5 +103,31 @@ public class Animal : MonoBehaviourPun, IPunObservable
         {
             curState = (State)(int)stream.ReceiveNext();
         }
+    }
+
+    // test..
+    public bool isRun;
+    private void OnDrawGizmosSelected()
+    {
+        if (isRun)
+            return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, searchRadius);
+
+        Gizmos.color = Color.green;
+        Vector3 angleDir1 = transform.TransformDirection(new Vector3(Mathf.Sin(Mathf.Deg2Rad * searchAngle),
+            0, Mathf.Cos(Mathf.Deg2Rad * searchAngle)));
+        Vector3 angleDir2 = transform.TransformDirection(new Vector3(Mathf.Sin(Mathf.Deg2Rad * -searchAngle),
+            0, Mathf.Cos(Mathf.Deg2Rad * -searchAngle)));
+        Vector3 angleDir3 = transform.TransformDirection(new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * -searchAngle),
+             Mathf.Cos(Mathf.Deg2Rad * -searchAngle)));
+        Vector3 angleDir4 = transform.TransformDirection(new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * searchAngle),
+             Mathf.Cos(Mathf.Deg2Rad * searchAngle)));
+
+        Gizmos.DrawRay(transform.position, angleDir1 * searchRadius);
+        Gizmos.DrawRay(transform.position, angleDir2 * searchRadius);
+        Gizmos.DrawRay(transform.position, angleDir3 * searchRadius);
+        Gizmos.DrawRay(transform.position, angleDir4 * searchRadius);
     }
 }
--- Assets/1bongsu/Scripts/Animal/State/AnimalState.cs
+++ Assets/1bongsu/Scripts/Animal/State/AnimalState.cs
@@ -5,30 +5,69 @@ public class AnimalState : BaseState<Animal.State>
 {
     protected Animal owner;
 
+    private Coroutine searchRoutine;
+
     protected void SetDestination(Vector3 nextPoint) // 다음 목적지를 정해주기
     {
         owner.Agent.SetDestination(nextPoint);
+    }
+
+    protected void StartSearchRoutine()
+    {
+        if (searchRoutine != null)
+            return;
+
+        searchRoutine = owner.StartCoroutine(owner.SearchRoutine()); // 탐색루틴 실행
+    }
+    protected void StopSearchRoutine()
+    {
+        if (searchRoutine == null)
+            return;
+
+        owner.StopCoroutine(searchRoutine);
+        searchRoutine = null;
     }
 }
 
 #region Chicken
 public class C_IdleState : AnimalState
 {
+    Coroutine idleRoutine;
+
     public override void Enter()
     {
         owner.CurState = Animal.State.Idle;
 
         int rand = Random.Range(0, 2);
-        owner.Animator.SetInteger("RandomPattern", rand);
-        owner.StartCoroutine(IdleRoutine());
+        owner.Animator.SetInteger("RandomPattern", rand); // 대기동작 2가지중 한가지 실행
+        idleRoutine = owner.StartCoroutine(IdleRoutine());
+
+        StartSearchRoutine();
     }
 
     IEnumerator IdleRoutine()
     {
         // 지금 재생하는 애니메이션의 이름이 "Idle"이면
         yield return new WaitUntil(() => owner.Animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"));
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(3f);
         ChangeState(Animal.State.Roam);
+    }
+
+    public override void Exit()
+    {
+        if (idleRoutine == null)
+            return;
+
+        owner.StopCoroutine(idleRoutine); // 플레이어를 찾아서 RunState로 바뀌면 아이들 루틴 종료
+    }
+
+    public override void Transition()
+    {
+        if (owner.Target != null) // target이 지정되면 RunState로 상태변경
+        {
+            ChangeState(Animal.State.Run);
+            StopSearchRoutine();
+        }
     }
 
     public C_IdleState(Animal owner)
@@ -62,14 +101,6 @@ public class C_RoamState : AnimalState
         SetDestination(nextPoint);
     }
 
-    public override void Update()
-    {
-        if (Time.time - time > 7f)
-        {
-            SetDestination(owner.transform.position); // 가끔 네비 경계면에 지정을 해줘서 목적지에 도달하지 못한다
-        }
-    }
-
     public override void Exit()
     {
         owner.Animator.SetBool("IsRoaming", false);
@@ -77,7 +108,18 @@ public class C_RoamState : AnimalState
 
     public override void Transition()
     {
-        if ((nextPoint - owner.transform.position).sqrMagnitude < 0.01f)
+        if (owner.Target != null)
+        {
+            ChangeState(Animal.State.Run);
+            StopSearchRoutine();
+        }
+        // 가끔 네비 경계면에 지정을 해줘서 목적지에 도달하지 못한다, 그래서 일정 시간이 지나면 상태변경
+        else if (Time.time - time > 7f)
+        {
+            ChangeState(Animal.State.Idle);
+            SetDestination(owner.transform.position);
+        }
+        else if ((nextPoint - owner.transform.position).sqrMagnitude < 0.01f)
         {
             ChangeState(Animal.State.Idle);
         }
@@ -91,6 +133,47 @@ public class C_RoamState : AnimalState
 
 public class C_RunState : AnimalState
 {
+    private float time;
+    private float originSpd;
+
+    public override void Enter()
+    {
+        owner.CurState = Animal.State.Run;
+        owner.Animator.SetInteger("RandomPattern", 2); // 다른 애니메이션으로 넘어가지 않도록
+        owner.Animator.SetBool("IsRun", true);
+
+        owner.isRun = true; // test
+        time = Time.time;
+
+        Vector3 toRunDir = (owner.transform.position - owner.Target.position); // 타겟으로 부터 반대방향으로 도망
+        toRunDir = new Vector3(toRunDir.x, 0, toRunDir.z).normalized;
+        SetDestination(owner.transform.position + toRunDir * 4f);
+        originSpd = owner.Agent.speed;
+        owner.Agent.speed = 2f;
+    }
+
+    public override void Transition()
+    {
+        if (Time.time - time > 3f)
+        {
+            ChangeState(Animal.State.Idle);
+            SetDestination(owner.transform.position);
+        }
+        else if ((owner.Agent.destination - owner.transform.position).sqrMagnitude < 0.01f)
+        {
+            ChangeState(Animal.State.Idle);
+        }
+    }
+
+    public override void Exit()
+    {
+        owner.isRun = false; // test
+
+        owner.Target = null;
+        owner.Agent.speed = originSpd;
+        owner.Animator.SetBool("IsRun", false);
+    }
+
     public C_RunState(Animal owner)
     {
         this.owner = owner;
